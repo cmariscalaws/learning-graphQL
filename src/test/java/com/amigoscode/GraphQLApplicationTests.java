c6e68f4a39cb4f1eb003eaedff6d17453ea24814
“Carlos <cmariscalaws@gmail.com>
2025-07-09 15:56:28 -0700

    updated to use new training

--- src/test/java/com/amigoscode/GraphQLApplicationTests.java
+++ src/test/java/com/amigoscode/GraphQLApplicationTests.java
@@ -1,13 +1,13 @@
-package com.learning.graphql;
+package com.amigoscode;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GraphqlApplicationTests {
+class GraphQLApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
